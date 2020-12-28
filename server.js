d6e5386d62c263dcaebabc8f15654a07d3c45bdd
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-12-28 21:37:59 +0530

    shorten url redirect and increment in the clicks is added in server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app=express();
 const mongoose=require('mongoose');
 const shortenUrl = require('./models/databaseSchema');
 
+app.use(express.urlencoded({extended: false}));
+
+let urllg;
+
 mongoose.connect('mongodb+srv://user:tngzF5jkyoobC66d@cluster0.4qtzn.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology:true
@@ -18,5 +22,29 @@ app.get('/',(req,res)=>{
 });
 
 
+
+app.post('/shortenurl',async (req,res)=>{
+    const url=req.body.long;
+    await shortenUrl.create({long:req.body.long});
+    res.render('index');
+
+});
+
+
+app.get('/:shorturl',async (req,res)=>{
+    const urlsh=req.params.shorturl;
+    myresult=await shortenUrl.findOne({short:urlsh});
+    if(myresult==null){
+        res.sendStatus(404);
+    }
+    else{
+        urllg=myresult.long;
+        myresult.clicks++;
+        myresult.save();
+        
+    }
+    res.redirect(urllg);
+        
+});
 
 app.listen(5000);
