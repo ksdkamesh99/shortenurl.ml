3c786a3ec7dd32a823cf27a94c49d9abe0842b82
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-12-28 20:41:42 +0530

    Connected to MongoDB atlas

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const shortenUrl = require('./models/databaseSchema');
+
+mongoose.connect('mongodb+srv://user:tngzF5jkyoobC66d@cluster0.4qtzn.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology:true
+});
+
+
+
 
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     res.render('index')
 });
+
+
 
 app.listen(5000);
